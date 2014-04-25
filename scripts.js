06bc592d382be5a7ee317f3003ee29453b7711b2
Sylvain Dorey <dorey.sylvain@gmail.com>
2014-04-25 16:26:37 +0200

    add barycenter

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+var positions = [];
+var points_layer;
+
 var init = function() {
   map = new OpenLayers.Map("map");
   var base_layer = new OpenLayers.Layer.OSM();
@@ -9,5 +12,85 @@ var init = function() {
   var zoom = 13;
   map.addLayer(base_layer);
   map.setCenter(position, zoom);
+  points_layer = create_points_layer('Points Layer');
+  map.addLayer(points_layer);
 
+  var i = 0;
+
+  OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {                
+    defaultHandlerOptions: {
+        'single': true,
+        'double': false,
+        'pixelTolerance': 0,
+        'stopSingle': false,
+        'stopDouble': false
+    },
+
+    initialize: function(options) {
+        this.handlerOptions = OpenLayers.Util.extend(
+            {}, this.defaultHandlerOptions
+        );
+        OpenLayers.Control.prototype.initialize.apply(
+            this, arguments
+        ); 
+        this.handler = new OpenLayers.Handler.Click(
+            this, {
+                'click': this.trigger
+            }, this.handlerOptions
+        );
+    }, 
+
+    trigger: function(e) {
+        var lonlat = map.getLonLatFromPixel(e.xy);
+        positions[i] = {};
+        positions[i].lon = lonlat.lon;
+        positions[i].lat = lonlat.lat;
+        i++;
+        var point = new OpenLayers.Feature.Vector(new OpenLayers.Geometry.Point(lonlat.lon, lonlat.lat))
+        points_layer.addFeatures(point);
+    }
+  });
+
+  var click = new OpenLayers.Control.Click();
+  map.addControl(click);
+  click.activate();
+
+}
+
+
+//
+// create_points_layer
+//
+// Create the layer with the points and its style.
+
+var create_points_layer = function(name) {
+  var styleMap = new OpenLayers.StyleMap({
+    "default": new OpenLayers.Style({
+      "pointRadius": 6,
+      "strokeWidth": 1,
+      "fillColor": "red"
+    }),
+    "select": new OpenLayers.Style({
+    })
+  });
+
+  var layer = new OpenLayers.Layer.Vector(name, {
+      styleMap: styleMap,
+  });
+  return layer;
+}
+
+var get_bar = function(positions) {
+  var x_bar = 0;
+  var y_bar = 0;
+
+  for(var p = 0 ; p < positions.length ; p++) {
+    x_bar += positions[p].lon;
+    y_bar += positions[p].lat;
+  }
+
+  x_bar = x_bar / positions.length;
+  y_bar = y_bar / positions.length;
+  var barycentre = new OpenLayers.Feature.Vector(new OpenLayers.Geometry.Point(x_bar, y_bar))
+  points_layer.addFeatures(barycentre);
 }
